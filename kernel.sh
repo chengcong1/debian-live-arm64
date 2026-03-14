3e51764c8f15c26e1251c227fffe3414ac20f401
程枞 <441185003@qq.com>
2026-03-14 11:40:06 +0800

    1

---
+++ kernel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+mkdir kernel && cd kernel
+
+wget https://github.com/oras-project/oras/releases/download/v1.3.1/oras_1.3.1_linux_arm64.tar.gz
+tar -xzvf oras_1.3.1_linux_arm64.tar.gz
+chmod +x ./oras
+
+# ghcr.io=ghcr.nju.edu.cn、ghcr.m.daocloud.io、mirror.ghcr.io
+KERNELTAG=$(./oras repo tags ghcr.io/armbian/os/kernel-rockchip64-edge | grep "7.0" | tail -n 1)
+./oras pull ghcr.io/armbian/os/kernel-rockchip64-edge:$KERNELTAG
+
+tar -xf kernel-rockchip64-edge-$KERNELTAG.tar
+
+cp global/linux-image-edge-rockchip64_*.deb .
+cp global/linux-headers-edge-rockchip64_*.deb .
+cp global/linux-libc-dev-edge-rockchip64_*.deb .
--- lb-config-trixie.sh
+++ lb-config-trixie.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p live-build&& cd live-build
+
 # 默认配置
 MODE="debian"
 DISTRIBUTION="trixie"
@@ -41,8 +43,6 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-
-# 根据镜像源类型配置 apt 源
 configure_mirror() {
     case $MIRROR_TYPE in
         aliyun)
@@ -60,97 +60,66 @@ configure_mirror() {
     esac
 }
 
-# 执行镜像配置
+# execute
 configure_mirror
 
 LB_IMAGE_NAME="$MODE-$DISTRIBUTION-live" lb config \
     --mode $MODE \
-	--architecture $ARCH \
-	--debian-installer live \
-    --debian-installer-gui true \
-	--archive-areas 'main contrib non-free non-free-firmware' \
-	--parent-archive-areas 'main contrib non-free non-free-firmware' \
-	--debian-installer-distribution $DISTRIBUTION \
-	--distribution $DISTRIBUTION \
-	--distribution-chroot $DISTRIBUTION \
-	--distribution-binary $DISTRIBUTION\
-	--keyring-packages "debian-archive-keyring ca-certificates fontconfig-config initramfs-tools" \
-	--parent-mirror-bootstrap $MIRROR_BASE \
-	--parent-mirror-chroot $MIRROR_BASE \
-	--parent-mirror-chroot-security $MIRROR_SECURITY \
-	--parent-mirror-binary $MIRROR_BASE \
-	--parent-mirror-binary-security $MIRROR_SECURITY \
-	--parent-mirror-debian-installer $MIRROR_BASE \
-	--mirror-bootstrap $MIRROR_BASE \
-	--mirror-chroot $MIRROR_BASE \
-	--mirror-chroot-security $MIRROR_SECURITY \
-	--mirror-binary $MIRROR_BASE \
-	--mirror-binary-security $MIRROR_BASE \
-	--mirror-debian-installer $MIRROR_BASE \
-    --bootappend-live "boot=live components quiet" \
-	--binary-images iso-hybrid \
-	--bootloaders grub-efi \
+    --architecture $ARCH \
+    --archive-areas "main contrib non-free non-free-firmware" \
+    --parent-archive-areas "main contrib non-free non-free-firmware" \
+    --debian-installer-distribution $DISTRIBUTION \
+    --distribution $DISTRIBUTION \
+    --distribution-chroot $DISTRIBUTION \
+    --distribution-binary $DISTRIBUTION\
+    --keyring-packages "debian-archive-keyring ca-certificates fontconfig-config initramfs-tools" \
+    --parent-mirror-bootstrap $MIRROR_BASE \
+    --parent-mirror-chroot $MIRROR_BASE \
+    --parent-mirror-chroot-security $MIRROR_SECURITY \
+    --parent-mirror-binary $MIRROR_BASE \
+    --parent-mirror-binary-security $MIRROR_SECURITY \
+    --parent-mirror-debian-installer $MIRROR_BASE \
+    --mirror-bootstrap $MIRROR_BASE \
+    --mirror-chroot $MIRROR_BASE \
+    --mirror-chroot-security $MIRROR_SECURITY \
+    --mirror-binary $MIRROR_BASE \
+    --mirror-binary-security $MIRROR_BASE \
+    --mirror-debian-installer $MIRROR_BASE \
+    --bootappend-live "boot=live components quiet locales=zh_CN.UTF-8" \
+    --binary-images iso-hybrid \
+    --bootloaders grub-efi \
     --firmware-binary false \
     --firmware-chroot false \
-	--apt-secure false \
-	--updates true \
+    --apt-secure false \
+    --updates true
     # --cache-packages true \
     # --cache-stages bootstrap,chroot
-	# --linux-packages "linux-image linux-dtb linux-headers" \
-	# --linux-flavours "legacy-rk35xx" \
+    # --debian-installer live \
+    # --debian-installer-gui true \
+    # --linux-packages "linux-image linux-dtb linux-headers" \
+    # --linux-flavours "legacy-rk35xx" \
 
-# package-lists 存放需要apt安装的包
-# 添加桌面环境包 
-cat > config/package-lists/desktop.list.chroot << EOF
-xfce4
-xfce4-goodies
-xfce4-terminal
-xfce4-power-manager
-xfce4-screenshooter
-lightdm
-calamares
+# package-lists need to apt install packages in chroot
+cp ../addpackage-custom.list.chroot config/package-lists/
+cp ../addpackage-kde.list.chroot config/package-lists/
+# add other live packages 
+cat > config/package-lists/livepkg.list.chroot << EOF
+grub-efi-arm64
 calamares-settings-debian
 live-task-localisation
 live-task-recommended
-
-grub-efi-arm64
 systemd-timesyncd
 EOF
 
-cat > config/package-lists/system.list.chroot << EOF
-mesa-utils
-firmware-misc-nonfree
-nano
-htop
-gparted
-locales
-network-manager
-network-manager-gnome
-pulseaudio
-alsa-utils
-pavucontrol
-bluetooth
-wget
-openssh-client
-fonts-wqy-zenhei
-EOF
+# copy custom kernel in config/packages.chroot/
+# no apt packages in config/packages.chroot/ auto install
 
 
-# wget https://github.com/chengcong1/debian-live-arm64/releases/download/kernel_linux-7.0-rc2/linux-headers-7.0.0-rc2-moli-arm64.deb   -O config/package/linux-headers-arm64.deb
-# wget https://github.com/chengcong1/debian-live-arm64/releases/download/kernel_linux-7.0-rc2/linux-image-7.0.0-rc2-moli-arm64.deb     -O config/package/linux-image-arm64.deb
-# wget https://github.com/chengcong1/debian-live-arm64/releases/download/kernel_linux-7.0-rc2/linux-libc-dev_7.0.0-rc2-moli-arm64.deb  -O config/package/linux-libc-dev-arm64.deb
-
-# 创建目录结构
-mkdir -p config/includes.chroot/opt/custom-kernel
-
-# 复制内核 deb 包到该目录
-cp ../kernel/linux-image-*.deb config/includes.chroot/opt/custom-kernel/
-cp ../kernel/linux-headers-*.deb config/includes.chroot/opt/custom-kernel/
-cp ../kernel/linux-libc-dev-*.deb config/includes.chroot/opt/custom-kernel/
-
-cp ../install-custom-kernel.hook.chroot config/hooks/live/
-chmod +x config/hooks/chroot/install-custom-kernel.chroot
+# hooks remove-default-kernel
+cp ../0090-remove-default-kernel.hook.chroot config/hooks/live/
+chmod +x config/hooks/live/0090-remove-default-kernel.hook.chroot
 
 # 复制rtl8852be的固件到firmware目录
-cp -r ../rtw89 config/includes.chroot/usr/lib/firmware/
+# cp -r ../rtw89 config/includes.chroot/usr/lib/firmware/
 
+sudo lb build
